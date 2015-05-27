beb9d8a7babfdbaa5d49b20dcbb3425b68c2ae28
Ubuntu <datastax@bhess-a1.bhess-a1.g9.internal.cloudapp.net>
2015-05-27 15:23:55 +0000

    cql

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 odbcsql: odbcsql.c
 	gcc -o odbcsql odbcsql.c -lodbc
 
+cql: cql.c
+	gcc -o cql cql.c -lcassandra
---
+++ cql.c
@@ -0,0 +1,220 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "cassandra.h"
+
+#define TRYCASS(x)   {   CassError rc = x;			\
+  if (rc != CASS_OK)						\
+    {								\
+      fprintf(stderr, "ERROR: %s\n", cass_error_desc(rc));	\
+      exit(-1);							\
+    }								\
+  }
+
+void print_error(CassFuture* future) {
+  const char* message;
+  size_t message_length;
+  cass_future_error_message(future, &message, &message_length);
+  fprintf(stderr, "Error: %.*s\n", (int)message_length, message);
+}
+
+CassCluster* create_cluster(const char* contact_points) {
+  CassCluster* cluster = cass_cluster_new();
+  cass_cluster_set_contact_points(cluster, contact_points);
+  return cluster;
+}
+
+CassError connect_session(CassSession* session, const CassCluster* cluster) {
+  CassError rc = CASS_OK;
+  CassFuture* future = cass_session_connect(session, cluster);
+
+  cass_future_wait(future);
+  rc = cass_future_error_code(future);
+  if (rc != CASS_OK) {
+    print_error(future);
+  }
+  cass_future_free(future);
+
+  return rc;
+}
+
+CassError execute_query(CassSession* session, const char* query) {
+  CassError rc = CASS_OK;
+  CassFuture* future = NULL;
+  CassStatement* statement = cass_statement_new(query, 0);
+
+  future = cass_session_execute(session, statement);
+  cass_future_wait(future);
+
+  rc = cass_future_error_code(future);
+  if (rc != CASS_OK) {
+    print_error(future);
+  }  
+
+  cass_future_free(future);
+  cass_statement_free(statement);
+  
+  return rc;
+}
+
+char* get_column_as_string(const CassValue *value, char* buf, int bufsize) {
+  buf[0] = '\0';
+  const char *tbuf;
+  char *tbuf2;
+  size_t tbufsize;
+  CassValueType vtype = cass_value_type(value);
+  cass_int64_t val_int64;
+  cass_int32_t val_int32;
+  cass_float_t val_float;
+  cass_double_t val_double;
+  cass_bool_t val_bool;
+  CassUuid val_uuid;
+  CassInet val_inet;
+  const cass_byte_t *val_byte;
+  
+  switch (vtype) {
+  case CASS_VALUE_TYPE_ASCII:
+  case CASS_VALUE_TYPE_TEXT:
+  case CASS_VALUE_TYPE_VARCHAR:
+    TRYCASS(cass_value_get_string(value, &tbuf, &tbufsize));
+    if (tbufsize < bufsize - 1) {
+      memcpy(buf, tbuf, tbufsize);
+      buf[tbufsize] = '\0';
+    }
+    break;
+  case CASS_VALUE_TYPE_BIGINT:
+  case CASS_VALUE_TYPE_TIMESTAMP:
+    TRYCASS(cass_value_get_int64(value, &val_int64));
+    sprintf(buf, "%lld", (long long)val_int64);
+    break;
+  case CASS_VALUE_TYPE_INT:
+    TRYCASS(cass_value_get_int32(value, &val_int32));
+    sprintf(buf, "%d", (int)val_int32);
+    break;
+  case CASS_VALUE_TYPE_FLOAT:
+    TRYCASS(cass_value_get_float(value, &val_float));
+    sprintf(buf, "%f", (float)val_float);
+    break;
+  case CASS_VALUE_TYPE_DOUBLE:
+    TRYCASS(cass_value_get_double(value, &val_double));
+    sprintf(buf, "%f", (double)val_double);
+    break;
+  case CASS_VALUE_TYPE_BOOLEAN:
+    TRYCASS(cass_value_get_bool(value, &val_bool));
+    sprintf(buf, "%s", (val_bool == cass_true) ? "1" : "0");
+    break;
+  case CASS_VALUE_TYPE_UUID:
+  case CASS_VALUE_TYPE_TIMEUUID:
+    TRYCASS(cass_value_get_uuid(value, &val_uuid));
+    cass_uuid_string(val_uuid, tbuf2);
+    sprintf(buf, "%s", tbuf2);
+    break;
+  case CASS_VALUE_TYPE_INET:
+    TRYCASS(cass_value_get_inet(value, &val_inet));
+    cass_inet_string(val_inet, tbuf2);
+    sprintf(buf, "%s", tbuf2);
+    break;
+  case CASS_VALUE_TYPE_BLOB:
+  case CASS_VALUE_TYPE_VARINT:
+  case CASS_VALUE_TYPE_LIST:
+  case CASS_VALUE_TYPE_MAP:
+  case CASS_VALUE_TYPE_SET:
+    TRYCASS(cass_value_get_bytes(value, &val_byte, &tbufsize));
+    if (tbufsize < bufsize - 1) {
+      memcpy(buf, val_byte, tbufsize);
+      buf[tbufsize] = '\0';
+    }
+    break;
+  default:
+    break;
+  }
+  return buf;
+}
+
+int main(int argc, char **argv) {
+  char *contact_points;
+  char *query;
+  bool silent = false;
+  
+  if ((argc != 3) && (argc != 4)) {
+    fprintf(stderr, "Usage: %s <contact_points> <Query> [silent]\n", argv[0]);
+    return 1;
+  }
+  contact_points = argv[1];
+  query = argv[2];
+  if (4 == argc) {
+    if (0 != strncmp("silent", argv[3], 6)) {
+      fprintf(stderr, "Usage: %s <ConnString> <Query> [silent]\n", argv[0]);
+      return 1;
+    }
+    else {
+      silent = true;
+    }
+  }
+
+  CassCluster* cluster = create_cluster(contact_points);
+  CassSession* session = cass_session_new();
+  CassFuture* close_future = NULL;
+
+  if (connect_session(session, cluster) != CASS_OK) {
+    cass_cluster_free(cluster);
+    cass_session_free(session);
+    return -1;
+  }
+
+  CassError rc = CASS_OK;
+  CassStatement* statement = NULL;
+  CassFuture* future = NULL;
+  long numResults = 0;
+  const char *tbuf;
+  size_t blen;
+  char buf[1025];
+
+  statement = cass_statement_new(query, 0);
+  future = cass_session_execute(session, statement);
+  cass_future_wait(future);
+
+  rc = cass_future_error_code(future);
+  if (rc != CASS_OK) {
+    print_error(future);
+  } 
+  else {
+    const CassResult* result = cass_future_get_result(future);
+    size_t nCols = cass_result_column_count(result);
+    size_t i;
+    CassIterator* iterator = cass_iterator_from_result(result);
+
+    while (cass_true == cass_iterator_next(iterator)) {
+      if (!silent) {
+	const CassRow* row = cass_iterator_get_row(iterator);
+	const CassValue* val = cass_row_get_column(row, 0);
+	printf("%s", get_column_as_string(val, buf, sizeof(buf)));
+	for (i = 1; i < nCols; i++) {
+	  val = cass_row_get_column(row, i);
+	  printf(",%s", get_column_as_string(val, buf, sizeof(buf)));
+	}
+	printf("\n");
+      }
+      numResults++;
+    }
+
+    cass_result_free(result);
+    cass_iterator_free(iterator);
+  }
+
+  fprintf(stderr, "numResults = %ld\n", numResults);
+
+  cass_future_free(future);
+  cass_statement_free(statement);
+
+  close_future = cass_session_close(session);
+  cass_future_wait(close_future);
+  cass_future_free(close_future);
+
+  cass_cluster_free(cluster);
+  cass_session_free(session);
+
+  return 0;
+}
